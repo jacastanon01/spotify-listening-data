d092c978ad05a6da20069337adacc262aa171036
Jacob Castanon <jacastanon01@gmail.com>
2024-06-04 22:33:09 -0500

    created module to write data to multiple sources

---
+++ load_history.py
@@ -0,0 +1,18 @@
+import os
+import sqlite3
+
+from config import PATH_TO_DATADIR
+from database import initialize_db, insert_into_db, reset_and_connect_db
+from normalize import process_listening_history, write_normalized_data_to_json_file
+
+
+if __name__ == "__main__":
+    conn = reset_and_connect_db()
+
+    path_to_json = "normalized-data/extracted_data.json"
+    data = process_listening_history(PATH_TO_DATADIR)
+    write_normalized_data_to_json_file(data, path_to_json)
+    for row in data:
+        insert_into_db(conn, row)
+
+    conn.close()
--- main.py
+++ main.py
@@ -1,19 +1,12 @@
 import sqlite3
 
 from config import PATH_TO_DATADIR
-from database import initialize_tables, insert_into_db
+from database import initialize_db, insert_into_db
 from normalize import process_listening_history
 
 
 def main():
-    conn = sqlite3.connect("data.db")
-    initialize_tables(conn)
-
-    data = process_listening_history(PATH_TO_DATADIR)
-    for row in data:
-        insert_into_db(conn, row)
-
-    conn.close()
+    pass
 
 
 if __name__ == "__main__":
